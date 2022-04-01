e376a2b34372cd85798ae2d58b1836290539e3a5
Pereira da Silva <m.f.pereira.da.silva@accenture.com>
2022-04-01 16:03:47 -0300

    Adicionada função calculo

--- functions/script.js
+++ functions/script.js
@@ -11,3 +11,10 @@ function backspace(){
     let valor = document.getElementById('visor').innerHTML;
     document.getElementById('visor').innerHTML = valor.substring(0,valor.length -1);
 }
+
+function calculo(){
+    let resultado = document.getElementById('visor').innerHTML;
+    if(visor){
+        document.getElementById('visor').innerHTML = eval(resultado);
+    }
+}
